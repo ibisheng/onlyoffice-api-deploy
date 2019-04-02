0e82ba40df7f564d4e6b52ff8b7246edc2741575
dan <xiexyz@gmail.com>
2019-04-02 16:42:32 +0800

    fix

---
+++ initAdminPass.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+if [ ! -n "$1" ] ;then
+    echo "请输入控制台初始化密码"
+    exit
+fi
+
+
+var=$(cat .config)
+arr=()
+for element in $var
+do
+    arr[${#arr[*]}]=$element
+done
+echo ${arr[0]} ${arr[1]};
+data=${arr[0]}
+tag=${arr[1]}
+
+docker rm tools -f 1 > /dev/null 2>&1
+
+docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/tools:$tag
+
+docker run --name tools --privileged  \
+    -v $3:/workspace \
+    -e RUN_ENV=docker \
+    -e type=32  \
+    -e pass=$1
+    --net=bisheng  \
+    registry.cn-zhangjiakou.aliyuncs.com/bisheng/tools:$tag
