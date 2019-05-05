23082bf343c55f0e92c13c6892d09814892ef1a3
dan <xiexyz@gmail.com>
2019-05-05 11:36:26 +0800

    添加字体

--- fontsService.sh
+++ fontsService.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+
+if [  -n "$1" ] ;then
+    echo "你指定了字体目录，请确保该目录下有字体文件，同时请确保你对这些字体拥有版权"
+    if [ ! -d userFonts  ];then
+        mkdir userFonts
+    fi
+    cp $1/* userFonts
+fi
+
 var=$(cat .config)
 arr=()
 for element in $var
@@ -12,6 +21,8 @@ tag=${arr[1]}
 rm -rf $data/workspace/fonts/*
 
 cp -r workspace/fonts/* $data/workspace/fonts
+cp -r userFonts/* $data/workspace/fonts
+
 
 bash init.sh 64 $tag $data
 
